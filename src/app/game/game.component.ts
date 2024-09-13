83e257af2f3b08d43d79a5750e789d5c73315b18
Konstantin Aksenov <kav.pbg@gmail.com>
2024-09-13 17:45:00 -0400

    new loom

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss',
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game: any;
+
+  constructor() {
+    // this.game = Game
+    this.newGame();
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
     console.log('Linke Maustaste wurde geklickt.');
     this.pickCardAnimation = true;
+  }
+
+  calcPositionDieErstenVier(i: number) {
+    return -i * 16 + 65 + 'px';
   }
 }
