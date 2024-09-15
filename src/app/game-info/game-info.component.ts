79ab5fff4db8df76ff5d636802f7b2b7ba0efde8
Konstantin Aksenov <kav.pbg@gmail.com>
2024-09-15 17:30:43 -0400

    loom video

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -1,4 +1,10 @@
-import { Component, Injectable } from '@angular/core';
+import {
+  Component,
+  Injectable,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Injectable({
@@ -11,4 +17,88 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './game-info.component.html',
   styleUrl: './game-info.component.scss',
 })
-export class GameInfoComponent {}
+export class GameInfoComponent implements OnChanges {
+// export class GameInfoComponent {
+  title: string = '';
+  description: string = '';
+  @Input() card: string = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+  // ngOnInit(changes: SimpleChanges): void {
+  console.log("In der OnChanges fn drinne!");
+  
+    if (changes['card'] && changes['card'].currentValue) {
+      console.log(this.card);
+      const cardNumber = +this.card.split('_')[1];
+      if (this.cardAction[cardNumber - 1]) {
+        this.title = this.cardAction[cardNumber - 1].title;
+        this.description = this.cardAction[cardNumber - 1].description;
+      }
+    }
+  }
+  // ngOnChanges(): void {
+  //   console.log(this.card);
+  //   if (this.card) {
+  //     let cardNumber = +this.card.split('_')[1];
+  //     this.title = this.cardAction[cardNumber - 1].title;
+  //     this.description = this.cardAction[cardNumber - 1].description;
+  //     console.log(cardNumber);
+  //   }
+  // }
+
+  cardAction = [
+    {
+      title: 'Waterfall',
+      description:
+        'All players start drinking at the same time. The player who drew the card can stop drinking whenever they choose, but the next person in line cannot stop until the first player does, and so on.',
+    },
+    {
+      title: 'You',
+      description:
+        'The player who drew the card picks someone to take a drink.',
+    },
+    { title: 'Me', description: 'The player who drew the card takes a drink.' },
+    {
+      title: 'Floor',
+      description:
+        'All players must touch the floor as quickly as possible. The last person to do so drinks.',
+    },
+    { title: 'Guys', description: 'All male players drink.' },
+    { title: 'Chicks', description: 'All female players drink.' },
+    {
+      title: 'Heaven',
+      description:
+        'All players must point to the sky. The last person to do so drinks.',
+    },
+    {
+      title: 'Mate',
+      description:
+        "The player who drew the card picks a 'mate.' Whenever one of them drinks, the other must drink as well.",
+    },
+    {
+      title: 'Rhyme',
+      description:
+        'The player who drew the card says a word, and the next player must say a word that rhymes with it. This continues around the circle until someone either repeats a word or can`t think of a rhyme. That person drinks.',
+    },
+    {
+      title: 'Categories',
+      description:
+        "The player who drew the card picks a category (e.g., types of cars, pizza toppings). Players take turns naming something that fits in that category. The first person who can't think of something drinks.",
+    },
+    {
+      title: 'Make a Rule',
+      description:
+        'The player who draws a Jack gets to make a rule that stays in effect for the rest of the game. Anyone who breaks the rule drinks.',
+    },
+    {
+      title: 'Question Master',
+      description:
+        'The player who draws the Queen becomes the Question Master. Whenever they ask another player a question, the other player must answer with another question. If a player fails to answer with a question, they drink.',
+    },
+    {
+      title: 'King`s Cup',
+      description:
+        "The first three Kings require the player to pour some of their drink into the King's Cup. The player who draws the fourth King must drink the entire King`s Cup, ending the game.",
+    },
+  ];
+}
